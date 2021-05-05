835d9251c17137fab477b38dd8671e69e833f8f9
Sandra george <sandra@rckr.com>
2021-05-05 18:09:50 +0530

    Commit Add project files.

---
+++ ApplicationEndpoints/Controllers/StaffController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ApplicationEndpoints.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+       
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public ActionResult<Staff> Get()
+        {
+
+            // fetch files from csv file
+            try
+            {
+                return Ok(StaffDetailsFromFile.getStaffDetailsFromFile());
+
+            }
+            catch (Exception exception)
+            {
+                return NotFound();
+            }
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ApplicationEndpoints/Staff.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ApplicationEndpoints
+{
+    public class Staff
+    {
+
+        public int id { get; set; }
+
+        public string firstname { get; set; }
+
+        public string lastname { get; set; }
+        public string emailId { get; set; }
+
+        public string role { get; set; }
+
+       public Staff()
+        {
+
+        }
+
+        public Staff(int id, string firstname, string lastname, string emailId, string role)
+        {
+            this.id = id;
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.emailId = emailId;
+            this.role = role;
+
+
+        }
+    }
+}
